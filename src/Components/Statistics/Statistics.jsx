dcbbeb4bd575e6a6441075507a8a4a6bd50387c7
zannatin-tazri <zannatintazrizan@gmail.com>
2023-04-11 02:29:21 +0600

    dashboard created of my assignment marks

--- src/Components/Statistics/Statistics.jsx
+++ src/Components/Statistics/Statistics.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
-
+import './statistics.css'
+import {
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip
+  } from "recharts";
 const Statistics = () => {
+    const assignment_dashboard=[
+        {
+            Assignment_No : "Assignment 1",
+            Mark:60
+        },
+        {
+            Assignment_No : "Assignment 2",
+            Mark:57
+        },
+        {
+            Assignment_No : "Assignment 3",
+            Mark:58
+        },
+        {
+            Assignment_No : "Assignment 4",
+            Mark:51
+        },
+        {
+            Assignment_No : "Assignment 5",
+            Mark:50
+        },
+        {
+            Assignment_No : "Assignment 6",
+            Mark:49
+        },
+        {
+            Assignment_No : "Assignment 7",
+            Mark:60
+        },
+        {
+            Assignment_No : "Assignment 8",
+            Mark:35
+        }
+    ];
     return (
-        <div>
-            <h1>hi</h1>
+        <div className='dashboard'>
+            
+           <AreaChart
+           width={1000}
+           height={300}
+           data={assignment_dashboard}
+           >
+            <Area type="monotone" dataKey="Mark" stroke="#8884d8" fill="#8884d8" />
+            <XAxis dataKey="Assignment_No" />
+            <YAxis></YAxis>
+            <Tooltip></Tooltip>
+           </AreaChart>
         </div>
     );
 };
