1cba61e7334b385fc65cf7e5c103396366d94490
zannatin-tazri <zannatintazrizan@gmail.com>
2023-04-12 16:00:34 +0600

    final commit

--- src/Components/404page/Error404page.jsx
+++ src/Components/404page/Error404page.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Error404page.css'
+import { Link } from 'react-router-dom';
 
 const Error404page = () => {
     return (
         <div className='Error404page'>
              <h1>Oops!</h1>
-             <h1>404</h1>
+             <h2>404</h2>
              <h2>Page not found !!!</h2>
-             <h2><Link to="/home">Back to Home Page</Link> </h2>
+             <p><Link to="/"> Back to home page</Link></p>
+            
         </div>
     );
 };
